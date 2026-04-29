33a2ad1c99f8547ee4bc51cfdf096885937a289d
kerenie-cmd <kerenieineza47@gmail.com>
2026-04-29 22:31:02 +0200

    Add daily life JavaScript program

---
+++ dailyLife.js
@@ -0,0 +1,25 @@
+let name = 'kerenie';
+let age = 17;
+let favoriteFood = 'pizza';
+let studyHoursPerDay = 5;
+let isStudent = true;
+
+// Part 2: Simple Calculations
+let studyHoursPerWeek = studyHoursPerDay * 7;
+let ageInMonths = age * 12;
+
+// Bonus: Age in dog years (1 human year = 7 dog years)
+let ageInDogYears = age * 7;
+
+// Part 3: Output
+console.log("Hello, my name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("I love eating " + favoriteFood + ".");
+console.log(
+  "I study " +
+    studyHoursPerDay +
+    " hours per day, which is " +
+    studyHoursPerWeek +
+    " hours per week."
+);
+console.log("I am " + ageInMonths + " months old.");
